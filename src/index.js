2a8a737a8a6a397af461620ba626d1efc41f9d87
Carolina Ferreira <carolina.bf@hotmail.com>
2020-05-29 16:11:16 +0100

    start auth

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './core/index.css';
 import App from './core/App';
-import * as serviceWorker from '../core/serviceWorker';
+import * as serviceWorker from './core/serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
